ff31c5e47e6eaff4244e3be2f684bcf12fde3d68
Andreas Christianson <achristianson@pessimistic-it.com>
2021-10-06 23:17:07 -0500

    nested, indented svg elements

--- src/files/write-to-stream.js
+++ src/files/write-to-stream.js
@@ -1,9 +1,10 @@
 import log from 'npmlog';
 import { getWriteStream } from './index.js';
 
-export const writeToStream = async (string) =>
+export const writeToStream = async (string, depth = 0) =>
   new Promise((resolve) => {
-    if (getWriteStream().write(`${string}\n`)) {
+    const indent = '  '.repeat(depth);
+    if (getWriteStream().write(`${indent}${string}\n`)) {
       resolve();
     } else {
       log.silly('fs buffer full. Pausing for drain event.');
--- src/plans/plan-utils.js
+++ src/plans/plan-utils.js
@@ -2,45 +2,57 @@ import log from 'npmlog';
 import { writeToStream } from '../files/index.js';
 import pack from '../../package.json';
 
-const isIterable = (value) => typeof value!=="string"&& Symbol.iterator in Object(value);
+const isIterable = (value) => typeof value !== "string" && Symbol.iterator in Object(value);
 
-const objectToAttributes = (object, separator = ' ') =>
+const objectToAttributes = (object = {}, separator = ' ') =>
   Object.entries(object)
     .map(([key, value]) => [key, isIterable(value) ? value.join('\n') : value])
     .map(([key, value]) => `${key}="${value}"`)
     .join(separator);
 
-const writeElement = async (element) => {
+
+const writeElement = async (element, depth = 0) => {
+  if (isIterable(element)) {
+    return Promise.all(element.map(subElement => writeElement(subElement, depth)));
+  } 
   const attributes = objectToAttributes(element.attributes);
+  const closingTag = element.closingTag || `</${element.tag}>`
+  const openingTag = element.openingTag ||`<${element.tag} ${attributes}>`
   if (!element.content) {
-    await writeToStream(`<${element.tag} ${attributes}/>`);
+    await writeToStream(`${openingTag.slice(0,-1)}/>`, depth);
+  } else if (typeof element.content === 'string') {
+    await writeToStream(`${openingTag}${element.content}${closingTag}`, depth);
   } else {
-    await writeToStream(
-      `<${element.tag} ${attributes}>${element.content}</${element.tag}>`
-    );
+    await writeToStream(`${openingTag}`, depth);
+    await writeElement(element.content, depth + 1);
+    await writeToStream(closingTag, depth);
   }
 };
 export const generateSvgBuilder =
   (elementGenerator, attributeOverrides = {}) =>
-  async (options) => {
-    const attributes = {
-      width: options.width,
-      height: options.height,
-      version: '1.1',
-      xmlns: 'http://www.w3.org/2000/svg',
-      ...attributeOverrides,
+    async (options) => {
+      const attributes = {
+        width: options.width,
+        height: options.height,
+        version: '1.1',
+        xmlns: 'http://www.w3.org/2000/svg',
+        ...attributeOverrides,
+      };
+      await writeElement({
+        tag: 'svg',
+        attributes,
+        content: [
+          {
+            openingTag: '<!--',
+            closingTag: '-->',
+            content:`\n${pack.description}\n${pack.homepage}\n${pack.version}\n`
+          },
+          {
+            openingTag: '<!--',
+            closingTag: '-->',
+            content: `\nParameters:\n${JSON.stringify(options, null,'  ')}\n`
+          },
+          ...await elementGenerator(options)
+        ]
+      })
     };
-    await writeToStream(`<svg ${objectToAttributes(attributes)} >`);
-    await writeToStream(
-      `<!-- created with https://github.com/AndreasChristianson/theory-of-colours version ${pack.version}`
-    );
-    await writeToStream(objectToAttributes(options, '\n'));
-    await writeToStream(`-->`);
-
-    const elements = await elementGenerator(options);
-    for (const element of elements) {
-      await writeElement(element);
-    }
-
-    await writeToStream('</svg>');
-  };
